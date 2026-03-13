48fd54589959282f095f6c358fe260d9b2902121
nikolozi2001 <mr.qachibaia44@gmail.com>
2026-03-13 15:47:14 +0400

    feat: enhance getMarriagesByYear to support optional male and female age group filters

--- controllers/marriages.controller.js
+++ controllers/marriages.controller.js
@@ -3,7 +3,7 @@ const db = require("../config/db.config");
 // Get marriages by year
 exports.getMarriagesByYear = async (req, res) => {
   try {
-    const { year } = req.query;
+    const { year, maleAgeGroup, femaleAgeGroup } = req.query;
     if (!year) {
       return res.status(400).json({
         error: {
@@ -13,21 +13,32 @@ exports.getMarriagesByYear = async (req, res) => {
       });
     }
 
-    const query = `
+    const params = [year];
+    let query = `
       SELECT [Id], [Year], [MaleAgeGroup], [FemaleAgeGroup], [MarriageCount]
       FROM [pyramid].[dbo].[Marriages]
       WHERE [Year] = ?
     `;
 
-    console.log("Executing query:", query, "with year:", year);
-    const [results] = await db.query(query, [year]);
+    if (maleAgeGroup) {
+      query += ` AND [MaleAgeGroup] = ?`;
+      params.push(maleAgeGroup);
+    }
+
+    if (femaleAgeGroup) {
+      query += ` AND [FemaleAgeGroup] = ?`;
+      params.push(femaleAgeGroup);
+    }
+
+    console.log("Executing query:", query, "with params:", params);
+    const [results] = await db.query(query, params);
 
     if (!results || results.length === 0) {
       return res.status(404).json({
         error: {
-          message: `No data found for year ${year}`,
+          message: `No data found for the given filters`,
           status: 404,
-          filters: { year },
+          filters: { year, maleAgeGroup, femaleAgeGroup },
         },
       });
     }
